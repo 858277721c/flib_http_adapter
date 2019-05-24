59889d50bf9ce987e28980ae3ae07742241f819a
zjun <565061763@qq.com>
2019-05-24 18:04:38 +0800

    FHttpRequest

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -2,6 +2,41 @@ import 'package:meta/meta.dart';
 
 import 'exceptions.dart';
 
+class FHttpManager {
+  static final FHttpManager singleton = FHttpManager._();
+
+  FHttpManager._();
+
+  bool _init = false;
+
+  FHttpAdapter _adapter;
+
+  FHttpAdapter get adapter {
+    _checkInit();
+    return _adapter;
+  }
+
+  /// 初始化
+  void init({@required FHttpAdapter adapter}) {
+    assert(adapter != null);
+    _adapter = adapter;
+    _init = true;
+  }
+
+  void _checkInit() {
+    if (!_init) {
+      throw Exception(
+          'You must invoke FHttpManager.singleton.init() method before this');
+    }
+  }
+}
+
+abstract class FHttpAdapter {
+  Future<FHttpResponse> toResponse(FHttpRequestInfo requestInfo);
+
+  void handleHttpException(dynamic e);
+}
+
 enum FHttpMethod {
   post,
   get,
@@ -11,39 +46,44 @@ enum FHttpMethod {
 /// http发起请求的一些信息
 class FHttpRequestInfo {
   final String path;
-  final Map<String, dynamic> params;
+  final Map<String, dynamic> queryParams;
+  final dynamic data;
   final FHttpMethod method;
 
   FHttpRequestInfo({
     this.path,
-    this.params,
+    this.queryParams,
+    this.data,
     this.method,
   }) : assert(method != null);
 }
 
 /// http请求返回信息
-class FHttpResponse<T> {
+class FHttpResponse {
   final int code;
-  final T content;
+  final dynamic data;
 
   FHttpResponse({
     @required this.code,
-    @required this.content,
+    @required this.data,
   }) : assert(code != null);
 }
 
-abstract class FHttpRequest<T> {
-  FHttpResponse<T> _response;
+abstract class FHttpRequest {
+  FHttpResponse _response;
 
-  FHttpResponse<T> get response => _response;
+  FHttpResponse get response => _response;
 
   /// 发起请求
-  Future<FHttpResponse<T>> toResponse() async {
-    FHttpResponse<T> response;
+  Future<FHttpResponse> toResponse() async {
+    FHttpManager.singleton._checkInit();
+
+    FHttpResponse response;
 
     final FHttpRequestInfo requestInfo = FHttpRequestInfo(
       path: getPath(),
-      params: getParams(),
+      queryParams: getQueryParams(),
+      data: getData(),
       method: getMethod(),
     );
 
@@ -55,6 +95,15 @@ abstract class FHttpRequest<T> {
     }
 
     assert(response != null);
+
+    final Type type = getExpectedResponseDataType();
+    if (type != null) {
+      if (type != response.data.runtimeType) {
+        throw Exception(
+            'Expected response data type:${type} but ${response.data.runtimeType} was found');
+      }
+    }
+
     _response = response;
     return response;
   }
@@ -65,26 +114,48 @@ abstract class FHttpRequest<T> {
   }
 
   /// 请求参数
-  Map<String, dynamic> getParams();
+  Map<String, dynamic> getQueryParams() {
+    return null;
+  }
+
+  /// 附加数据，用来扩展各种不同的[FHttpMethod]所需要的参数
+  dynamic getData() {
+    return null;
+  }
 
   /// 请求方法
   FHttpMethod getMethod() {
     return FHttpMethod.post;
   }
 
-  @protected
-  Future<FHttpResponse<T>> toResponseImpl(FHttpRequestInfo request);
+  /// 返回希望得到的请求结果数据类型[FHttpResponse.data]
+  Type getExpectedResponseDataType() {
+    return String;
+  }
 
-  void handleHttpException(dynamic e) {}
+  @protected
+  Future<FHttpResponse> toResponseImpl(FHttpRequestInfo requestInfo) {
+    final FHttpAdapter adapter = FHttpManager.singleton.adapter;
+    return adapter.toResponse(requestInfo);
+  }
+
+  void handleHttpException(dynamic e) {
+    FHttpManager.singleton.adapter.handleHttpException(e);
+  }
 }
 
-abstract class FHttpStringRequest extends FHttpRequest<String> {}
+abstract class FHttpModelRequest<T> extends FHttpRequest {
+  @override
+  Type getExpectedResponseDataType() {
+    return String;
+  }
 
-abstract class FHttpModelRequest<T> extends FHttpStringRequest {
   /// 发起请求
   Future<T> toModel() async {
-    final FHttpResponse<String> response = await toResponse();
-    final String data = beforeParse(response.content);
+    final FHttpResponse response = await toResponse();
+
+    assert(response.data is String);
+    final String data = beforeParse(response.data);
 
     T model;
     try {
