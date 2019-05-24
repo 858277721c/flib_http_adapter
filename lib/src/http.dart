c7d7bdf6cf950c07a5ccf81e2dbb1f616124f91d
zjun <565061763@qq.com>
2019-05-24 16:36:16 +0800

    FHttpModelRequest

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -2,37 +2,63 @@ import 'package:meta/meta.dart';
 
 import 'exceptions.dart';
 
-class FHttpResponse {
-  final int httpCode;
-  final String content;
+/// http请求返回信息
+class FHttpResponse<T> {
+  final int code;
+  final T content;
 
   FHttpResponse({
-    @required this.httpCode,
+    @required this.code,
     @required this.content,
-  });
+  }) : assert(code != null);
 }
 
-abstract class FHttpRequest {
-  Future<FHttpResponse> toResponse() async {
-    final FHttpResponse response = await toResponseImpl();
+abstract class FHttpRequest<T> {
+  FHttpResponse<T> _response;
+
+  FHttpResponse<T> get response => _response;
+
+  /// 发起请求
+  Future<FHttpResponse<T>> toResponse() async {
+    final FHttpResponse<T> response = await toResponseImpl();
     assert(response != null);
+    _response = response;
     return response;
   }
 
   @protected
-  Future<FHttpResponse> toResponseImpl();
+  Future<FHttpResponse<T>> toResponseImpl();
 }
 
-abstract class FModelHttpRequest<T> extends FHttpRequest {
+abstract class FHttpStringRequest extends FHttpRequest<String> {}
+
+abstract class FHttpModelRequest<T> extends FHttpStringRequest {
+  /// 发起请求
   Future<T> toModel() async {
-    final FHttpResponse response = await toResponse();
+    final FHttpResponse<String> response = await toResponse();
+    final String data = beforeParse(response.content);
+
+    T model;
     try {
-      return parseToModel(response);
+      model = parseToModel(data);
     } catch (e) {
       throw new FHttpParseResponseException(error: e);
     }
+
+    assert(model != null);
+    afterParse(model);
+    return model;
   }
 
+  /// 请求内容转model之前调用
+  String beforeParse(String data) {
+    return data;
+  }
+
+  /// 将请求内容转为model
   @protected
-  T parseToModel(FHttpResponse response);
+  T parseToModel(String data);
+
+  /// 请求内容转model之后调用
+  void afterParse(T model) {}
 }
